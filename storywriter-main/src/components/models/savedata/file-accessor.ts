ef094490b7bb822bb299e328572a2f0a59ebd8e7
Yakumo Karasuma <yakumo.karasuma@gmail.com>
2021-11-15 19:08:36 +0900

    セーブ時にデータを圧縮するよう変更

--- storywriter-main/src/components/models/savedata/file-accessor.ts
+++ storywriter-main/src/components/models/savedata/file-accessor.ts
@@ -1,10 +1,14 @@
 import { promises as fs } from 'fs';
+import { ContentCompressor } from './content-compressor';
 
 export class FileAccessor {
+    static readonly prefix: string = "THIS_IS_SOMEONE'S_BRILLIANT_STORY";
+
     static async Save(path: string, content: string): Promise<FileAccessStatus> {
         const result = new FileAccessStatus();
         try {
-            await fs.writeFile(path, content);
+            const compressed = ContentCompressor.pack(FileAccessor.prefix + content);
+            await fs.writeFile(path, compressed);
             result.successed(`Write successfully completed to: "${path}" !!!`);
         } catch(err) {
             result.failed(err as string);
@@ -15,7 +19,14 @@ export class FileAccessor {
     static async Load(path: string): Promise<FileAccessStatus> {
         const result = new FileAccessStatus();
         try {
-            result.successed(await fs.readFile(path, "utf-8"));
+            const data = await fs.readFile(path, "utf-8");
+            const decompressed = ContentCompressor.unpack(data);
+            if(decompressed.indexOf(FileAccessor.prefix) != -1) {
+                result.successed(decompressed.replace(FileAccessor.prefix, ""));
+            } else {
+                throw Error("Invalid story data: Possibly this is not my story data...");
+            }
+            
         } catch(err) {
             result.failed(err as string);
         }
