d3677619635598c16872c617e4503aec5868345f
Yakumo Karasuma <yakumo.karasuma@gmail.com>
2021-11-22 21:59:37 +0900

    Build succeeded!

--- storywriter-main/src/background.ts
+++ storywriter-main/src/background.ts
@@ -27,7 +27,8 @@ async function createWindow() {
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: (process.env
           .ELECTRON_NODE_INTEGRATION as unknown) as boolean,
-      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION,
+      contextIsolation: !(process.env
+          .ELECTRON_NODE_INTEGRATION as unknown) as boolean,
       enableRemoteModule: true
     }
   })
